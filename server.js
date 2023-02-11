798fae477c59beaeef7fb30654bde22da5079c4d
Vikasgupta04 <vg482002@gmail.com>
2023-02-11 09:23:17 +0530

    hi

---
+++ server.js
@@ -0,0 +1,6 @@
+// node server.js
+let a=[1,2,3,4,5,6,7,8,9,10];
+for (let index = 0; index < a.length; index++) {
+    const element = a[index];
+    console.log("index:"+index+"\tvalue:"+element);
+}
